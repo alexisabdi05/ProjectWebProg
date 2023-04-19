af182fd886e75cd04cbdc8a6e1e5d734529f3412
Alex <alexisabdi02@gmail.com>
2023-04-19 10:58:25 +0700

    push1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hi Welcome Back to Laravel</title>
+        <title>Laravel</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
