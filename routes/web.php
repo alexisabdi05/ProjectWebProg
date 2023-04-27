7bcedbc9f23879d3cadb9e6e8148f6519a65e343
Nataliakrndng <natalia.karundeng7878@gmail.com>
2023-04-27 20:33:13 +0700

    pushtata

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/signin', function () {
+    return view('signin');
+});
+
+Route::get('/signup', function () {
+    return view('signup');
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
