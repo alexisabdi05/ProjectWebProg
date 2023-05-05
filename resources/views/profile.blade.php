ea27d08618b4146d9a7bfe860c2c9ad8a1115614
Nataliakrndng <natalia.karundeng7878@gmail.com>
2023-05-05 09:48:52 +0700

    1

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -19,11 +19,22 @@
         <h1 class="text-6xl font-extrabold ml-12">Hello Manda!</h1>
         <p class="text-lg ml-12">It's good to see you again.</p>
     </div>
-    <div class="w-15 md:w-1/5 h-64 bg-white shadow-lg rounded-xl ml-3"></div>
-    <div class="w-15 md:w-1/5 h-64 bg-white shadow-lg rounded-xl"></div>
+    <div class="w-15 md:w-1/5 h-64 bg-white shadow-lg rounded-xl ml-3 flex flex-col justify-center content-center items-center">
+        <h1 class="text-6xl font-extrabold content-center">3</h1>
+        <p class="mt-3 text-lg text-center leading-snug content-center">Courses<br>Completed</p>
+    </div>
+    <div class="w-15 md:w-1/5 h-64 bg-white shadow-lg rounded-xl flex flex-col justify-center content-center items-center">
+        <h1 class="text-6xl font-extrabold content-center">5</h1>
+        <p class="mt-3 text-lg text-center leading-snug content-center">Courses<br>In Progress</p>
+    </div>
   </div>
   <div>
     <img src="{{ asset('img/profile.png') }}" class="img-profile">
+    </div>
+  </div>
+  <div class="continue">
+    <div class="w-full h-30 bg-white shadow-lg rounded-2xl flex flex-col justify-center items-start">
+
     </div>
   </div>
   @include('layout/footer')
