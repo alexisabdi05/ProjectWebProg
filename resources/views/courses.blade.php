915644f998b2c27595c901373786ded05edf5fc1
tctatxt <tammyvisca08@gmail.com>
2023-07-04 22:08:05 +0700

    update enrollment

--- resources/views/courses.blade.php
+++ resources/views/courses.blade.php
@@ -1,33 +1,32 @@
-
 @extends('master')
-@section('Title','Courses')
-@section('Style','/css/courses.css')
+@section('Title', 'Courses')
+@section('Style', '/css/courses.css')
 @section('content')
 
-<div class="hero">
-    @include('include.form')
-    <div class="flex justify-center">
-        <img src="/{{ $category->CategoryIMG}}" alt="" class="main">
+    <div class="hero">
+        @include('include.form')
+        <div class="flex justify-center">
+            <img src="/{{ $category->CategoryIMG }}" alt="" class="main">
+        </div>
+        <h1 class="title">{{ $category->CategoryName }}</h1>
     </div>
-    <h1 class="title">{{ $category->CategoryName }}</h1>
-</div>
 
-<div class="courses">
-@foreach ($courses as $course )
-<a href="/{{ $user->id }}/courses/{{ $course ->id}}">
-    <div class="course-list">
-        <div  class="flex justify-center">
-            <img src="/img/course-cover.png" alt="">
-        </div>
-        {{-- <img src="{{ $course->courseIMG }}" alt=""> --}}
-        <div class="course-txt">
-            <div class="flex items-center cont">
-        <h2 class="course-title">{{ $course->CourseName }}</h2>
+    <div class="courses">
+        @foreach ($courses as $course)
+            <a href="/courses/{{ $course->id }}">
+                <div class="course-list">
+                    <div class="flex justify-center">
+                        <img src="/img/course-cover.png" alt="">
+                    </div>
+                    {{-- <img src="{{ $course->courseIMG }}" alt=""> --}}
+                    <div class="course-txt">
+                        <div class="flex items-center cont">
+                            <h2 class="course-title">{{ $course->CourseName }}</h2>
+                        </div>
+                        <h4 class="course-desc">{{ $course->CourseDesc }}</h4>
+                        <p class="more text-center">See more →</p>
+                    </div>
+                </div>
+            </a>
+        @endforeach
     </div>
-        <h4 class="course-desc">{{ $course->CourseDesc }}</h4>
-        <p class="more text-center">See more →</p>
-        </div>
-    </div>
-</a>
-@endforeach
-</div>
